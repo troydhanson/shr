0cf876c73a9a2f8ef05806cfc6216aa5aad609a9
Troy D. Hanson <tdh@tkhanson.net>
2017-08-28 13:33:06 +0000

    tee mode

--- util/shr-tool.c
+++ util/shr-tool.c
@@ -17,7 +17,7 @@ struct {
   char *prog;
   int verbose;
   char *ring;
-  enum {mode_status, mode_create, mode_write, mode_read} mode;
+  enum {mode_status, mode_create, mode_write, mode_read, mode_tee} mode;
   struct shr *shr;
   size_t size;
   int flags;
@@ -38,6 +38,9 @@ void usage() {
                  "\n"
                  "[write mode ]: -w <ring>\n"
                  "  writes data to ring, from lines of stdin until eof\n"
+                 "\n"
+                 "[tee mode ]: -t <input-ring> <output-ring> <output-ring>\n"
+                 "  tee's one ring into two output rings\n"
                  "\n"
                  "[create mode]: -c -s <size> [-m <mode>] <ring> [<ring> ...]\n"
                  "  create ring(s) of given size and mode\n"
@@ -77,10 +80,12 @@ int main(int argc, char *argv[]) {
   CF.prog = argv[0];
   char unit, *c, buf[10000], *app_data, line[1000];
   struct shr_stat stat;
+  struct shr *o1=NULL, *o2=NULL;
+  char *ring1=NULL, *ring2=NULL;
   size_t app_len;
   ssize_t nr;
 
-  while ( (opt = getopt(argc,argv,"vhcs:qm:wrb:")) > 0) {
+  while ( (opt = getopt(argc,argv,"vhcs:qm:wrtb:")) > 0) {
     switch(opt) {
       case 'v': CF.verbose++; break;
       case 'h': default: usage(); break;
@@ -89,6 +94,7 @@ int main(int argc, char *argv[]) {
       case 'q': CF.mode=mode_status; break;
       case 'w': CF.mode=mode_write; break;
       case 'r': CF.mode=mode_read; break;
+      case 't': CF.mode=mode_tee; break;
       case 's':  /* ring size */
          sc = sscanf(optarg, "%ld%c", &CF.size, &unit);
          if (sc == 0) usage();
@@ -123,6 +129,35 @@ int main(int argc, char *argv[]) {
   if (CF.ring == NULL) usage();
   
   switch(CF.mode) {
+
+    case mode_tee:
+      CF.shr = shr_open(CF.ring, SHR_RDONLY | SHR_GET_APPDATA, 
+                        &app_data, &app_len);
+      if (CF.shr == NULL) goto done;
+      if (++optind < argc) ring1 = argv[optind];
+      if (++optind < argc) ring2 = argv[optind];
+      if ((ring1 == NULL) || (ring2 == NULL)) goto done;
+      o1 = shr_open(ring1, SHR_WRONLY);
+      o2 = shr_open(ring2, SHR_WRONLY);
+      if ((o1 == NULL) || (o2 == NULL)) goto done;
+      while(1) {
+        nr = shr_read(CF.shr, buf, sizeof(buf));
+        if (nr <= 0) {
+          fprintf(stderr, "shr_read: %s (%ld)\n", 
+             (nr == 0) ? "end-of-data" : "error", nr); 
+          goto done;
+        } else {
+          if (shr_write(o1, buf, nr) < 0) {
+            fprintf(stderr, "shr_write: error\n");
+            goto done;
+          }
+          if (shr_write(o2, buf, nr) < 0) {
+            fprintf(stderr, "shr_write: error\n");
+            goto done;
+          }
+        }
+      }
+      break;
 
     case mode_create:
       if (CF.size == 0) usage();
@@ -194,5 +229,7 @@ int main(int argc, char *argv[]) {
  
  done:
   if (CF.shr) shr_close(CF.shr);
-  return 0;
+  if (o1) shr_close(o1);
+  if (o2) shr_close(o2);
+  return rc;
 }
